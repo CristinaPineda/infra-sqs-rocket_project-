7a0460b60d39484dc5c4755ee5903b248c984837
CristinaPineda <cristinadaspineda@yahoo.com.br>
2025-08-24 17:19:25 -0300

    :star: ajuste codigos e workflow limpo

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,9 +1,9 @@
-output "sqs_arn" {
+output "sqs_queue_arn" {
   description = "The ARN of the SQS queue."
-  value       = aws_sqs_queue.sns_target_queue.arn
+  value       = aws_sqs_queue.rocket_project_sqs.arn
 }
 
 output "sqs_queue_name" {
   description = "The name of the SQS queue."
-  value       = aws_sqs_queue.sns_target_queue.name
+  value       = aws_sqs_queue.rocket_project_sqs.name
 }
