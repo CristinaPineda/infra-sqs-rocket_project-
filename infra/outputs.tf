e7c8feee8a21f08c91ebc9cf415bd2f54ecc2c66
CristinaPineda <cristinadaspineda@yahoo.com.br>
2025-08-23 21:29:24 -0300

    :star: ajuste outputs

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,9 +1,9 @@
 output "sqs_arn" {
   description = "The ARN of the SQS queue."
-  value       = aws_sqs_queue.rocket_project_sqs_queue.arn
+  value       = aws_sqs_queue.sns_target_queue.arn
 }
 
 output "sqs_queue_name" {
   description = "The name of the SQS queue."
-  value       = aws_sqs_queue.rocket_project_sqs_queue.name
+  value       = aws_sqs_queue.sns_target_queue.name
 }
